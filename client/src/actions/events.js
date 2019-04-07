ab5bc345ce40ee7e1c56050702db71271dbf8626
Sandra Padua <sandrapadua5@gmail.com>
2019-04-07 23:00:18 +0200

    solved issues of creating events tickets and comments

--- client/src/actions/events.js
+++ client/src/actions/events.js
@@ -4,7 +4,6 @@ import {logout} from './users'
 import {isExpired} from '../jwt'
 
 export const FETCHED_ALL_EVENTS = 'FETCHED_ALL_EVENTS'
-export const LOAD_EVENT_DETAILS = 'LOAD_EVENT_DETAILS'
 export const ADD_EVENT = 'ADD_EVENT'
 
 
@@ -13,11 +12,6 @@ const loadEvents = (events) => ({
   payload: events.events
 })
 
-const loadEventDetails = (event) => ({
-
-  type: LOAD_EVENT_DETAILS,
-  payload: event
-})
 
 
 export const fetchAllEvents = () => (dispatch) => {
@@ -27,15 +21,7 @@ export const fetchAllEvents = () => (dispatch) => {
     .then(result => console.log(dispatch(loadEvents(result.body))))
     .catch(err => console.error(err))
 }
-export const getEventDetails = (eventId) => (dispatch,getState) => {
-  console.log("get event details in action", eventId)
-  request
-    .get(`${baseUrl}/events/${eventId}`)
-    .then(response =>{
-      console.log("Fetched",response.body)
-       dispatch(loadEventDetails(response.body))})
-    .catch(err => console.error(err))
-}
+
 
 
 export const createEvent = (event) => (dispatch, getState) => {
--- client/src/store.js
+++ client/src/store.js
@@ -1,27 +1,11 @@
-// import {createStore, applyMiddleware, combineReducers, compose} from 'redux'
-// import ReduxThunk from 'redux-thunk'
-// import reducers from './reducers'
-// import {storeJwt} from './middleware'
 
-// const reducer = combineReducers(reducers)
 
-// const devTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-
-// const enhancer = compose(
-//   applyMiddleware(ReduxThunk, storeJwt),
-//   devTools
-// )
-
-// const store = createStore(reducer, enhancer)
-
-// export default store
-
-import { createStore, applyMiddleware, compose,combineReducers} from 'redux'
+import { createStore, applyMiddleware, compose} from 'redux'
 import reducers from './reducers'
 import ReduxThunk from 'redux-thunk'
 import {storeJwt} from './middleware'
 
-const reducer = combineReducers(reducers)
+// const reducer = combineReducers(reducers)
 const devTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 
 
